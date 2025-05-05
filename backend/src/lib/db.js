150e92524f48ac9a389e9b9ce6c51b1e141a613e
LuanCalais <luancalais@gmail.com>
2025-05-05 15:47:56 -0300

    fix: enviroment access

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
+      console.log(process.env.MONGODB_URI)
         const res = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Connected to mongoDB ${res.connection.host}`);
     } catch (error) {
