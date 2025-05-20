4955510f826503993ddec254782db155c018461e
LuanCalais <luancalais@gmail.com>
2025-05-20 11:39:18 -0300

    auth endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudnary from "../lib/cloudnary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
@@ -44,11 +45,70 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email })
+    if (!user) return res.status(400).json({ message: "Invalid user" })
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) res.status(400).json({ message: "Invalid user" })
+
+    generateToken(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    })
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 })
+    res.status(200).json({ message: "Logout successful" })
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+
+  }
 };
+
+export const updateUserProfile = async (req, res) => {
+  try {
+
+    const { profilePic } = req.body
+    const userId = req.user._id
+
+    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
+
+    const uplodaResponse = await cloludary.uploader.upload(profilePic)
+    const updateUser = await User.findByIdAndUpdate(userId, { profilePic: uplodaResponse.secure_url }, { new: true })
+    res.status(200).json(updateUser)
+
+  } catch (error) {
+    console.log("Error in updateUserProfile controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+
+  }
+}
+
+export const checkAuth = (req, res) => {
+
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+
+}
 
