70f8440460ad28e06e52105c1cc41d420293b3e1
LuanCalais <luancalais@gmail.com>
2025-06-02 12:06:26 -0300

    fix: imports

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudnary from "../lib/cloudnary.js";
 
