d624ef6bcde163cc74e03225be61ebc537211645
LuanCalais <luancalais@gmail.com>
2025-06-04 17:10:26 -0300

    fix: some integration fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   useEffect(() => {
     checkAuth();
     console.log(authUser);
-  }, [checkAuth]);
+  }, [checkAuth, authUser]);
 
   return (
     <div>
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "localhost:5001/api", withCredentials: true,
+    baseURL: "http://localhost:5001/api", withCredentials: true,
 })
